4526fc0aed1dd271cd8141d21d3ff6b0bce4ce32
siddartha8699 <51985403+siddartha8699@users.noreply.github.com>
2020-05-29 23:25:09 -0400

    Add files via upload

---
+++ Route.scala
@@ -0,0 +1,3 @@
+package ca.mcit.bg
+
+case class Route(route_id:Int,agency_id:String,route_short_name:Int,route_long_name:String,route_type:Int,route_url:String,route_colour:String,route_text_colour: Any)
