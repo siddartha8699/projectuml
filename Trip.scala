59fcbfd20077c2a34952799dde27bf585e17cfcb
siddartha8699 <51985403+siddartha8699@users.noreply.github.com>
2020-05-31 00:13:55 -0400

    Delete Trip.scala

--- Trip.scala
+++
@@ -1,3 +0,0 @@
-package ca.mcit.bg
-
-case class Trip(route_id:Int,service_id:String,trip_id :String,trip_headsign:String,direction_id:Int,shape_id:Int,wheelchair_accessible:Int,note_fr:String,note_en:String)
