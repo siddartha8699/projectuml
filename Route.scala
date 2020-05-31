0c92b0546ee382134a7bba1c4e6681dbc8892392
siddartha8699 <51985403+siddartha8699@users.noreply.github.com>
2020-05-31 00:13:34 -0400

    Delete Route.scala

--- Route.scala
+++
@@ -1,3 +0,0 @@
-package ca.mcit.bg
-
-case class Route(route_id:Int,agency_id:String,route_short_name:Int,route_long_name:String,route_type:Int,route_url:String,route_colour:String,route_text_colour: Any)
