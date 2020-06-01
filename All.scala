610ee2d5bd7eb6c98579a292e5606c2d39283f24
siddartha8699 <51985403+siddartha8699@users.noreply.github.com>
2020-06-01 12:16:10 -0400

    This is full length code

--- All.scala
+++ All.scala
@@ -5,10 +5,102 @@ import java.io.{BufferedWriter, FileWriter}
 
 import scala.io.Source
 
+case class TripTable(route_id: Int, service_id: String, trip_id: String, trip_headsign: String, direction_id: Int, shape_id: Int, wheelchair_accessible: Int, note_fr: String, note_en: String) {
+  def convertList(c : TripTable ) : List[String]={
+    var  out = List[String]()
+
+    out = c.route_id.toString :: c.service_id.toString :: c.trip_id.toString :: c.trip_headsign.toString :: c.direction_id.toString :: c.shape_id.toString :: c.wheelchair_accessible.toString :: c.note_fr.toString :: c.note_en.toString :: out
+    out
+  }
+}
+
+//case class is used here to hold the schema
+
+
+case class RouteTable(route_id: Int, agency_id: String, route_short_name: String, route_long_name: String, route_type: String, route_url: String, route_color: String, route_text_color: String){
+  def convertList(c : RouteTable ) : List[String]={
+    var  out = List[String]()
+
+    out = c.route_id.toString :: c.agency_id.toString :: c.route_short_name.toString :: c.route_long_name.toString :: c.route_type.toString :: c.route_url.toString :: c.route_color.toString :: c.route_text_color.toString :: out
+    out
+  }
+}
+
+case class CalenderTable(service_id:String,monday:Int,tuesday:Int,wednesday:Int,thursday:Int,friday:Int,saturday:Int,sunday:Int,start_date:Int,end_date:Int){
+  def convertList(c : CalenderTable ) : List[String]={
+    var  out = List[String]()
+    out = c.service_id.toString :: c.monday.toString :: c.tuesday.toString :: c.wednesday.toString :: c.thursday.toString :: c.friday.toString :: c.saturday.toString :: c.sunday.toString :: c.start_date.toString :: c.end_date.toString :: out
+    out
+  }
+}
+
+case class FinalEnrich(ServiceId: String, route: RouteTable, trip : TripTable, cal : CalenderTable)
+
+
+case class LookupTableForRoute(routes: List[RouteTable]) {
+  private val lookupTable: Map[Int, RouteTable] =
+    routes.map(route => route.route_id -> route).toMap
+  def lookup(routeId: Int): RouteTable = lookupTable.getOrElse(routeId, null)
+}
+case class LookupTableForCalender(calenders : List[CalenderTable]) {
+  private val lookupTable: Map[String, CalenderTable] =
+    calenders.map(calendar => calendar.service_id -> calendar).toMap
+  def lookup(serviceId: String): CalenderTable = lookupTable.getOrElse(serviceId, null)
+}
+
+
+
+
+
 
 object All extends App {
 
-  
+  def ReadingOfData(filePath: String): List[String] = {
+    Source.fromFile (filePath).getLines.toList
+  }
+
+  def apply_Routes(data : List[String]): List[RouteTable] = {
+    val valid_data = data.tail
+    var final_data = List[RouteTable]()
+    for(d <- valid_data){
+      var c = ""
+      if (d.takeRight(1) == ",") {
+        c = d.concat("-")   //null handling
+      }else{
+        c =d
+      }
+      var f =  c.split(",").toList
+      final_data = RouteTable (f (0).toInt, f (1).toString, f (2).toString, f (3).toString, f (4).toString, f (5).toString, f (6).toString, f (7).toString ) :: final_data
+    }
+    final_data
+  }
+
+  def apply_trips(data: List[String]) : List[TripTable] = {
+    val valid_data = data.tail
+    var final_data = List[TripTable]()
+    for(d <-valid_data){
+      var c = ""
+      if (d.takeRight(2) == ",,") {
+        c = d.replace(",,",",-,-")  //null handling
+      }else{
+        c =d
+      }
+      var f =  c.split(",").toList
+      final_data = TripTable (f (0).toInt, f (1).toString, f (2).toString, f (3).toString, f (4).toInt, f (5).toInt, f (6).toInt, f (7).toString,f(8).toString ) :: final_data
+    }
+    final_data
+  }
+
+  def apply_calendars(data: List[String]) : List[CalenderTable] = {
+    val valid_data = data.tail
+    var final_data = List[CalenderTable]()
+    for(d <-valid_data){
+      var f =  d.split(",").toList
+      final_data = CalenderTable (f (0).toString, f (1).toInt, f (2).toInt, f (3).toInt, f (4).toInt, f (5).toInt, f (6).toInt, f (7).toInt,f(8).toInt,f(9).toInt ) :: final_data
+    }
+    final_data
+  }
+
 
 
   val Routes = ReadingOfData("C:\\Users\\user\\Downloads\\gtfs_stm\\routes.txt")
